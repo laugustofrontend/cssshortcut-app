a9a4394e35105e1f0aa07387703c52d7539526ff
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-08-11 15:32:52 -0300

    terminado de fazer todo o planejamento de build do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,36 @@
 const gulp = require('gulp')
+const bs = require('browser-sync').create()
+const connect = require('gulp-connect')
+const pug = require('gulp-pug')
+const stylus = require('gulp-stylus')
+
+gulp.task('connect', () => {
+    connect.server({
+        root: './out',
+        livereload: true
+    })
+})
+
+gulp.task('pug', () => {
+  gulp.src('./src/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./out'))
+    .pipe(connect.reload())
+})
+
+gulp.task('stylus', () => {
+  gulp.src('./src/assets/styles/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./out/assets/styles'))
+    .pipe(connect.reload())
+})
+
+gulp.task('watch', () => {
+  gulp.watch(['./src/**/*.pug'], ['pug'])
+  gulp.watch(['./src/assets/styles/*.styl'], ['stylus'])
+})
+
+gulp.task('build', ['pug', 'stylus'])
+gulp.task('server', ['connect', 'watch'])
 
 gulp.task('default', () => {})
